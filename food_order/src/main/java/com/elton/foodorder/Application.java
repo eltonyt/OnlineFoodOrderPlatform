ab3091cc72b09495554af11c72a45c3157f2101d
Elton Li <elton.li@ubenefit.net>
2023-01-16 21:09:15 -0600

    Import Config File to Application File, Try not have multiple Configuration Files

--- food_order/src/main/java/com/elton/foodorder/Application.java
+++ food_order/src/main/java/com/elton/foodorder/Application.java
@@ -1,11 +1,14 @@
 package com.elton.foodorder;
 
+import com.elton.foodorder.config.WebMvcConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootApplication
+@Import(WebMvcConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- food_order/src/main/java/com/elton/foodorder/config/WebMvcConfig.java
+++ food_order/src/main/java/com/elton/foodorder/config/WebMvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
 @Slf4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
