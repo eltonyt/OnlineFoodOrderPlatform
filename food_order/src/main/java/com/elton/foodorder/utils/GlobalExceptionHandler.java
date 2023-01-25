f5c447f7cdd483cefec05c4ef5111d55bd01584b
Elton Li <elton.li@ubenefit.net>
2023-01-24 21:41:39 -0600

    Added Exception Handler - Handle Employee Username Duplication Exception

---
+++ food_order/src/main/java/com/elton/foodorder/utils/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.elton.foodorder.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException exception){
+        log.error(exception.getMessage());
+        if (exception.getMessage().contains("Duplicate entry")){
+            String[] split = exception.getMessage().split(" ");
+            String msg = split[2] + "already existed";
+            return R.error(msg);
+        }
+        return R.error("Unknown Error");
+    }
+}
