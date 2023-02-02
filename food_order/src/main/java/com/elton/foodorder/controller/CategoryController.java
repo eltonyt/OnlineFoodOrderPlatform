1b52d8ed2baa419f2e476e35b341fa0e721ee1d4
Elton Li <elton.li@ubenefit.net>
2023-02-01 23:35:24 -0600

    Added Category Saving Endpoint

--- food_order/src/main/java/com/elton/foodorder/controller/CategoryController.java
+++ food_order/src/main/java/com/elton/foodorder/controller/CategoryController.java
@@ -1,13 +1,25 @@
 package com.elton.foodorder.controller;
 
+import com.elton.foodorder.entity.Category;
 import com.elton.foodorder.service.CategoryService;
+import com.elton.foodorder.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/category")
+@Slf4j
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        categoryService.save(category);
+        return R.success("Category Added");
+    }
 }
